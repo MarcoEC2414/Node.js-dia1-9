cd4a1242cec1637f39aa38b8a6f3fd9aecd4f917
MarcoEC2414 <marcoescalante2414@gmail.com>
2026-03-25 08:24:45 -0500

    dia1

---
+++ app.js
@@ -0,0 +1,10 @@
+function array(){
+    let objeto = [
+        {nombre:"marco", edad:20},
+        {nombre:"princesa", edad:19}
+    ]
+    return objeto;
+}
+
+let usuarios = array();
+console.log(usuarios)
